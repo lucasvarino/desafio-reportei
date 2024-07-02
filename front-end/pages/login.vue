9cbf98934e3331197baa87fa5443d5c5d3852484
lucasvarino <lucasvarino@gmail.com>
2024-07-02 14:42:27 -0300

    feat: Login action in front and adding service env variables for github

--- front-end/pages/login.vue
+++ front-end/pages/login.vue
@@ -11,7 +11,7 @@
         Access your repository information and commit history by signing in with GitHub.
       </p>
       <div class="mt-6">
-        <Button class="inline-flex items-center justify-center rounded-md px-4 py-2 text-sm font-medium shadow-sm transition-colors hover:bg-primary/90 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2">
+        <Button @click="login" class="inline-flex items-center justify-center rounded-md px-4 py-2 text-sm font-medium shadow-sm transition-colors hover:bg-primary/90 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2">
           <GithubIcon iconClass="mr-2 h-5 w-5" />
           Sign in with GitHub
         </Button>
@@ -23,4 +23,8 @@
 <script setup>
 import Button from '@/components/ui/button/Button.vue'
 import GithubIcon from '@/components/icons/GithubIcon.vue'
+
+const login = () => {
+  window.location.href = 'http://localhost:8000/api/auth/github'
+}
 </script>
