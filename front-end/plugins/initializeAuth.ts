1c1b54b419517fc7548e84f90841ed201b36d723
lucasvarino <lucasvarino@gmail.com>
2024-07-03 18:15:32 -0300

    feat: Adding syncRepositories call to initializeAuthPlugin

--- front-end/plugins/initializeAuth.ts
+++ front-end/plugins/initializeAuth.ts
@@ -2,12 +2,14 @@ import { useUserStore } from '~/stores/user'
 
 export default defineNuxtPlugin(async () => {
     const userStore = useUserStore()
+    const repositoryStore = useRepositoryStore()
     const token = useCookie('token').value
 
     if (process.client && !userStore.user && token) {
         try {
             userStore.setToken(token)
             await userStore.fetchUser()
+            await repositoryStore.syncRepositories(token)
         } catch (error) {
             console.error('Failed to fetch user data', error)
         }
